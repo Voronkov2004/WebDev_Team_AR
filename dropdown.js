5ab2ecd717516c66a23a254bd6487abfd16255c0
Voronkov2004 <144661971+Voronkov2004@users.noreply.github.com>
2024-11-22 18:04:02 +0200

    Delete dropdown.js

--- dropdown.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.profile-icon').addEventListener('click', function(event) {
-    event.stopPropagation(); // Prevents click event from bubbling up
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-  });
